fcf50edda73654a28662a3b11e6c0bbc6266a53b
Helder Costa <heldercostaaa@gmail.com>
2025-01-31 23:35:05 -0300

    feat: connect cloudflare r2

--- src/env.ts
+++ src/env.ts
@@ -1,9 +1,19 @@
 import { z } from 'zod';
 
 const envSchema = z.object({
+  // Api
   NODE_ENV: z.enum(['development', 'test', 'production']).default('production'),
-  DB_URL: z.string().url().startsWith('postgresql://'),
   PORT: z.coerce.number().default(3333),
+
+  // Database
+  DB_URL: z.string().url().startsWith('postgresql://'),
+
+  // Cloudflare
+  CLOUDFLARE_ACCOUNT_ID: z.string(),
+  CLOUDFLARE_ACCESS_KEY_ID: z.string(),
+  CLOUDFLARE_SECRET_ACCESS_KEY: z.string(),
+  CLOUDFLARE_BUCKET: z.string(),
+  CLOUDFLARE_PUBLIC_URL: z.string().url(),
 });
 
 const _env = envSchema.safeParse(process.env);
